6076b584f1e3ce17d583f64fe18c57958276b41c
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-19 16:31:41 +0000

    Nice latex labels :)

--- plot_weekly_maxima.py
+++ plot_weekly_maxima.py
@@ -8,14 +8,18 @@ i_years = range(len(years))
 # months = ('11', '12', '01', '02', '03')
 months = ('05', '06', '07', '08', '09')
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('legend', fontsize='x-large')
+
 for y in i_years:
     dat_y = ()
     for m in months:
-        dat_y += tuple(read_data.extract_data(m, read_data.weekly_max, years[y])[0][:, 11, 11])
+        dat_y += tuple(read_data.extract_data(m, read_data.weekly_max, 'r', year=years[y])[0][:, 11, 11])
     plt.plot(range(1,21), dat_y)
     plt.title('Year '+str(years[y]))
     plt.xlabel('Week')
     plt.xticks(range(2, 21, 4), ('May', 'June', 'July', 'August', 'September'))
-    plt.ylabel('Weekly max rainfall')
-    plt.savefig('plots/weekly_max_rainfall_'+str(y)+'.png')
+    plt.ylabel(r'Weekly max rainfall ($kg\,m^{-2}$)')
+    plt.savefig('plots/weekly_max_rainfall_'+str(years[y])+'.png')
     plt.clf()

fa668e82425b0cb00ab81e7393fedef6976e2df1
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-19 14:44:44 +0000

    Some very basic test scripts

--- read_data.py
+++ read_data.py
@@ -5,6 +5,11 @@ from scipy.io.netcdf import NetCDFFile
 
 years = range(1979, 2011)
 
+spring_transition_months = ('04')
+warm_months = ('05', '06', '07', '08', '09')
+autumn_transition_months = ('10')
+cold_months = ('11', '12', '01', '02', '03')
+
 
 def daily_totals(rank3tensor):
     shape = np.shape(rank3tensor)
@@ -34,19 +39,28 @@ def loop_over_month(month_id, f):
         output.append(f(nc1.variables['A_PCP_L1_Accum_1']))
     return output
 
-# Show dimensions of data
-nc1 = NetCDFFile('data_sorted/CFSR_hourly_rainfall/cold/data1979_01.nc', mmap=False)
-# for key in nc1.variables.keys():
-#     print(key, np.shape(nc1.variables[key][:]))
-# test = daily_totals(nc1.variables['A_PCP_L1_Accum_1'])
-# print(test)
-# print(np.shape(test))
+if __name__ == '__main__':
 
-test2 = weekly_max(nc1.variables['A_PCP_L1_Accum_1'])
-print(test2)
-print(np.shape(test2))
+    # Test daily_totals
+    nc1 = NetCDFFile('data_sorted/CFSR_hourly_rainfall/cold/data1979_01.nc', mmap=False)
+    for key in nc1.variables.keys():
+        print(key, np.shape(nc1.variables[key][:]))
+    test = daily_totals(nc1.variables['A_PCP_L1_Accum_1'])
+    print(test)
+    print(np.shape(test))
+    if not bool(input('Test 1: Press 0 to exit, 1 to continue: ')):
+        exit(23)
 
-spring_transition_months = ('04')
-warm_months = ('05', '06', '07', '08', '09')
-autumn_transition_months = ('10')
-cold_months = ('11', '12', '01', '02', '03')
+    # Test daily_totals
+    test2 = weekly_max(nc1.variables['A_PCP_L1_Accum_1'])
+    print(test2)
+    print(np.shape(test2))
+    if not bool(input('Test 2: Press 0 to exit, 1 to continue: ')):
+        exit(23)
+
+    # Tet loop_over_month
+    test3 = loop_over_month('01', weekly_max)
+    print(test3)
+    print(np.shape(test3))
+    if not bool(input('Test 3: Press 0 to exit, 1 to continue: ')):
+        exit(23)

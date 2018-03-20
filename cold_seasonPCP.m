7fe3ac44a0765529b74b50ad4629563d0509f533
na517 <n.agarwal17@imeprial.ac.uk>
2018-03-20 11:05:17 +0000

    2 more seasons pcp data

---
+++ cold_seasonPCP.m
@@ -0,0 +1,14 @@
+%This script calculates total daily precipitation in UK in cold season
+year = 1979:2010;
+month=[1 2 3 11 12];
+for i = 1:length(year)
+    for j=1:length(month)
+        datafile = sprintf('data%d_%02d.nc',year(i),month(j));
+        data = ncread(datafile,'A_PCP_L1_Accum_1');
+        tot_time_steps = size(data,3);
+        total_days = tot_time_steps/24;
+        for k = 1:total_days
+            cold_pcp_data(i,j,k) = sum(sum(sum(data(:,:,((k-1)*24+1:k*24))))); 
+        end
+    end
+end

01ab7f1389c8089ad00eb0913c57b40b13ed1b12
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-20 10:57:00 +0000

    Niceties in plotting

--- plot_weekly_maxima.py
+++ plot_weekly_maxima.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import read_data
+
+from read_data import *
 
 
 years = range(1979, 2011)
 i_years = range(len(years))
-# months = ('11', '12', '01', '02', '03')
-months = ('05', '06', '07', '08', '09')
+months = ('11', '12', '01', '02', '03') if input('0: warm, 1: cold ') else ('05', '06', '07', '08', '09')
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -15,10 +14,10 @@ plt.rc('legend', fontsize='x-large')
 for y in i_years:
     dat_y = ()
     for m in months:
-        dat_y += tuple(read_data.extract_data(m, read_data.weekly_max, 'r', year=years[y])[0][:, 11, 11])
+        dat_y += tuple(extract_data(m, weekly_max, 'r', year=years[y])[0][:, 11, 11])
     plt.plot(range(1,21), dat_y)
     plt.title('Year '+str(years[y]))
-    plt.xlabel('Week')
+    plt.xlabel('')
     plt.xticks(range(2, 21, 4), ('May', 'June', 'July', 'August', 'September'))
     plt.ylabel(r'Weekly max rainfall ($kg\,m^{-2}$)')
     plt.savefig('plots/weekly_max_rainfall_'+str(years[y])+'.png')

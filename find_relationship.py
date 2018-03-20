e1fae767302b0f3c5ea2e46d9783876775362ad2
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-20 12:40:51 +0000

    Updated ratios

--- find_relationship.py
+++ find_relationship.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
 
-def take_ratio(season, field, location, takeMax=False):
+def take_ratio(season, field, location):
     x, y = location
 
-    dataDaily = pd.read_csv('daily_totals_%s_gp%d-%d_%s_takeMax=%s.csv' % (field, x, y, season, takeMax))
+    dataDaily = pd.read_csv('daily_totals_%s_gp%d-%d_%s_takeMax=True.csv' % (field, x, y, season))
     dataHourly = pd.read_csv('weekly_max_%s_gp%d-%d_%s.csv' % (field, x, y, season))
 
     shape = np.shape(dataDaily)
     assert (shape == np.shape(dataHourly))
     ratio = np.zeros(shape)
 
-    for i in shape[0]:
-        for j in shape[1]:
+    for i in range(shape[0]):
+        for j in range(shape[1]):
             ratio[i, j] = dataDaily[i, j] / dataHourly[i, j]
 
     df = pd.DataFrame(ratio)
     result = "ratio_%s_gp%s-%s_%s.csv" % (field, x, y, season)
     df.to_csv(result, index=False, mode="w")
+
+take_ratio('warm', 'r', (11, 11))

ef14ea959c985dfd3063c08979e171e803d10888
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-03-20 10:08:05 +0000

    Housekeeping

--- read_data.py
+++ read_data.py
@@ -9,7 +9,8 @@ years = range(1979, 2011)
 
 
 def extract_data(month_id, func, data_id, year=None):
-    # set data_id to 'u' (or 'v') for 'u' (or 'v') component of wind, or 'r' for rainfall
+    # set data_id to 'u' (or 'v') for 'u' (or 'v')
+    # component of wind, or 'r' for rainfall
     if month_id in ('11', '12', '01', '02', '03'):
         season = "cold/"
     elif month_id == '04':
@@ -24,7 +25,7 @@ def extract_data(month_id, func, data_id, year=None):
     if data_id == 'r':
         di = 'data_sorted/CFSR_hourly_rainfall/' + str(season)
     else:
-        di = 'data_sorted/CFSR_hourly_windspeed/' +str(season)
+        di = 'data_sorted/CFSR_hourly_windspeed/' + str(season)
 
     if year is None:
         for y in years:
@@ -81,27 +82,24 @@ def weekly_wind_average(rank3tensor):
     shape = np.shape(rank3tensor)
     days = int(shape[0]/24)
     weeks = int(days / 7)
-    D = np.zeros((weeks,shape[1],shape[2]))
+    D = np.zeros((weeks, shape[1], shape[2]))
+
     for i in range(weeks):
         for j in range(shape[1]):
                 for k in range(shape[2]):
-                    D[i, j, k] = np.mean([rank3tensor[l, j, k] for l in range(i*24*7, (i+1)*24*7)])
+                    D[i, j, k] = np.mean([rank3tensor[l, j, k]
+                                          for l in range(i*24*7, (i+1)*24*7)])
     return D
 
 
 def daily_wind_average(rank3tensor):
     shape = np.shape(rank3tensor)
     days = int(shape[0]/24)
-    D = np.zeros((days,shape[1],shape[2]))
+    D = np.zeros((days, shape[1], shape[2]))
 
     for i in range(days):
         for j in range(shape[1]):
                 for k in range(shape[2]):
-                    D[i, j, k] = np.mean([rank3tensor[l, j, k] for l in range(i*24, (i+1)*24)])
+                    D[i, j, k] = np.mean([rank3tensor[l, j, k]
+                                          for l in range(i*24, (i+1)*24)])
     return D
-
-
-# output0 = extract_data('04', weekly_max, year="1980")
-# output1 = extract_data('04', daily_totals, year="1980")
-# print(output0[0])
-# print(output1[0])
